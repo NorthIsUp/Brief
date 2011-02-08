12b8a208584e42c3d85fe3d7a6231e53649cc8f5
Toby Sterrett <tobysterrett@gmail.com>
2011-02-08 08:14:40 -0800

    replaced trombone and haha flash with audio tags

--- Brief.SkypeChatStyle/Contents/Resources/js/steezy.js
+++ Brief.SkypeChatStyle/Contents/Resources/js/steezy.js
@@ -25,55 +25,51 @@ var add_twitter_img_tags = function(message){
 };
 
 var add_sad_trombone = function(message) {
-  var the_match = message.match(/sadtrombone|wah/)
+  var the_match = message.match(/sadtrombone|wah|womp/)
   if (the_match) {
     var embed = ' '
-    embed += '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="244" height="152">'
-    embed += '<param name="movie" value="http://sadtrombone.com/sad_trombone.swf" />'
-    embed += '<param name="quality" value="high" />'
-    embed += '<param name="autoplay" value="true" />'
-    embed += '</object>'
+    embed += '<audio src="http://dl.dropbox.com/u/14572/steezy/Sad-Trombone.mp3" autoplay>'
+    embed += 'SAD TROMBONE PLAYING FOR YOU'
+    embed += '</audio>'
     return embed
   } else {
     return ''
   }
 };
-	
+
 var add_haha = function(message) {
   var the_match = message.match(/HAHA/)
   if (the_match) {
     var embed = ' '
-    embed += '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="400" height="373">'
-    embed += '<param name="movie" value="http://crossgrain.info/haha/haha.swf" />'
-    embed += '<param name="quality" value="high" />'
-    embed += '<embed src="http://crossgrain.info/haha/haha.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="400" height="373"></embed>'
-    embed += '</object>'
+    embed += '<audio src="http://dl.dropbox.com/u/14572/steezy/haha.mp3" autoplay>'
+    embed += 'NELSON SAYING HAHA'
+    embed += '</audio>'
     return embed
   } else {
     return ''
   }
 };
-	
+
 var add_emoticons = function(message) {
   var base = '<img src="http://l.yimg.com/us.yimg.com/i/mesg/emoticons7/'
   var end = '" />'
   var emoticonned = message
-	  .replace(/:'-?\(/g, base + '20.gif' + end)
-    .replace(/:-?\)/g, base + '1.gif' + end)
-    .replace(/:-?\(/g, base + '2.gif' + end)
-    .replace(/:-?D/g, base + '4.gif' + end)
-    .replace(/8-?\)/g, base + '16.gif' + end)
-    .replace(/\*facepalm\*/g, '<img src="http://img.skitch.com/20081020-kqf6ar41tdrwiqp2k6ejhr3q3t.jpg" alt="facepalm">')
-    .replace(/\dtmyk\d/ig, '<img src="http://img.skitch.com/20081202-mfnhnh9nursmcs2fb8smj6w94q.jpg" alt="tmyk">')
-    .replace(/the more you know/gi, '<img src="http://img.skitch.com/20081202-nr24rt47pu6d26y3qnmu994cha.jpg" alt="the more you know">')
-    .replace(/8=+(>|D)/g, '<img src="http://img.skitch.com/20080801-f2k6r13iaw7xsrya39ftamugaa.png" />')
-    .replace(/(DERP[!1]+)/g, '<img src="http://img.skitch.com/20080801-ehk4xc8n65xdx2sndc4scckyf2.jpg" alt="$1"/>')
-    .replace(/^\*?ba+r+f+s*\*?/g, '<img src="http://img.skitch.com/20080808-t8shmyktk66i65rx425jgrrwae.jpg" alt="Achewood - October 3, 2006"/>')
-    .replace(/do not want|\bdnw\b/gi, '<img src="http://img.skitch.com/20081031-1tt6dpsq42p85i2472xmc3yped.jpg" />')
-    .replace(/\bodb\b/gi, '<img src="http://img.skitch.com/20081211-gwjfs9q8gcqjdq3sn6w13bd4at.jpg" alt="oh baby I like it raw"/>')
+	  // .replace(/:'-?\\(/g, base + '20.gif' + end)
+    // .replace(/:-?\\)/g, base + '1.gif' + end)
+    // .replace(/:-?\\(/g, base + '2.gif' + end)
+    // .replace(/:-?D/g, base + '4.gif' + end)
+    // .replace(/8-?\\)/g, base + '16.gif' + end)
+    .replace(/\*facepalm\*/g, '<br><img src="http://img.skitch.com/20081020-kqf6ar41tdrwiqp2k6ejhr3q3t.jpg" alt="facepalm">')
+    .replace(/\dtmyk\d/ig, '<br><img src="http://img.skitch.com/20081202-mfnhnh9nursmcs2fb8smj6w94q.jpg" alt="tmyk">')
+    .replace(/the more you know/gi, '<br><img src="http://img.skitch.com/20081202-nr24rt47pu6d26y3qnmu994cha.jpg" alt="the more you know">')
+    .replace(/8=+(>|D)/g, '<br><img src="http://img.skitch.com/20080801-f2k6r13iaw7xsrya39ftamugaa.png" />')
+    .replace(/(DERP[!1]+)/g, '<br><img src="http://img.skitch.com/20080801-ehk4xc8n65xdx2sndc4scckyf2.jpg" alt="$1"/>')
+    .replace(/^\*?ba+r+f+s*\*?/g, '<br><img src="http://img.skitch.com/20080808-t8shmyktk66i65rx425jgrrwae.jpg" alt="Achewood - October 3, 2006"/>')
+    .replace(/do not want|\bdnw\b/gi, '<br><img src="http://img.skitch.com/20081031-1tt6dpsq42p85i2472xmc3yped.jpg" />')
+    .replace(/\bodb\b/gi, '<br><img src="http://img.skitch.com/20081211-gwjfs9q8gcqjdq3sn6w13bd4at.jpg" alt="oh baby I like it raw"/>')
   return emoticonned
 };
-	
+
 var add_youtube_embeds = function(message){
               var regex = /.*youtube\.com\/watch\?v=([a-zA-Z0-9\-]+).*?/;
   var the_match = message.match(regex);
@@ -124,13 +120,15 @@ var add_gists = function(message) {
 
 var oldAppendItem = SCS.conv.appendItem;
 SCS.conv.appendItem = function(html, scroll){
-  var extras = add_img_tags(html) 
-             + add_twitter_img_tags(html) 
-             + add_youtube_embeds(html) 
-             + add_vimeo_embeds(html) 
-             + add_sad_trombone(html) 
+  var extras = add_img_tags(html)
+             + add_twitter_img_tags(html)
+             + add_youtube_embeds(html)
+             + add_vimeo_embeds(html)
+             + add_sad_trombone(html)
+             + add_haha(html)
+             // + add_emoticons(html)
              + add_gists(html);
-             
+
   html = html.replace(/(<div class="body">.+)(<\/div>)/, "$1" + extras + "$2");
 
   return oldAppendItem(html, scroll);
