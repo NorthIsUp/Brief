59b3b40941298538b72359220b70d1f6e2896da1
3n <ian.collins@gmail.com>
2011-02-08 10:05:45 -0800

    process all urls

--- Brief.SkypeChatStyle/Contents/Resources/js/steezy.js
+++ Brief.SkypeChatStyle/Contents/Resources/js/steezy.js
@@ -4,8 +4,8 @@ SCS.conv._nearBottom = function(){
 
 var oldAppendItem = SCS.conv.appendItem;
 SCS.conv.appendItem = function(html, scroll){
-
-  var urlMatch = html.match(/(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?[\/\w\.-_]*/i);
+  var matchHtml = html.replace(/<[^>]+>/g,' '),
+      urlMatch = matchHtml.match(/(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?[\/\w\.-_]*/ig);
   if (urlMatch){
     var wrapper = new Element('div', {'html': html}),
         element = wrapper.getFirst(),
@@ -17,22 +17,24 @@ SCS.conv.appendItem = function(html, scroll){
     elementBody.grab(embedZone);
     html = wrapper.get('html');
 
-    new Request.JSONP({
-      url: 'http://api.embed.ly/1/oembed',
-      data: { 
-        url: urlMatch[0],
-        maxheight: 800,
-        maxwidth: 600
-      },
-      onComplete: function(data){
-        var elem = document.id(embedId);
-        if (elem.get('html').test(/Loading/)) elem.set('html','');
-        if (['video','rich'].contains(data.type))
-          elem.set('html', elem.get('html') + data.html);
-        else if (data.type == 'photo')
-          elem.set('html', elem.get('html') + '<img src="' + data.url + '">');
-      }
-    }).send();
+    urlMatch.each(function(url){
+      new Request.JSONP({
+        url: 'http://api.embed.ly/1/oembed',
+        data: { 
+          url: url,
+          maxheight: 800,
+          maxwidth: 600
+        },
+        onComplete: function(data){
+          var elem = document.id(embedId);
+          if (elem.get('html').test(/Loading/)) elem.set('html','');
+          if (['video','rich'].contains(data.type))
+            elem.set('html', elem.get('html') + data.html);
+          else if (data.type == 'photo')
+            elem.set('html', elem.get('html') + '<img src="' + data.url + '">');
+        }
+      }).send();
+    });
   }
 
   return oldAppendItem(html, scroll);
